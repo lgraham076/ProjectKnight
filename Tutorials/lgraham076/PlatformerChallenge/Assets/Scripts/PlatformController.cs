1c93c21b5b519455269b94fee837b77eb986fbd7
lgraham076 <lgraham076@gmail.com>
2016-07-27 21:35:25 -0500

    Created Initial Platformer Challenge Project
    Created platformer project to test learning developed from 2D Platformer
    tutorial

---
+++ Tutorials/lgraham076/PlatformerChallenge/Assets/Scripts/PlatformController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformController : MonoBehaviour {
+    [HideInInspector] public bool facingRight = true;
+    [HideInInspector] public bool jump = false;
+
+    public float moveForce = 365f;
+    public float maxSpeed = 5f;
+    public float jumpForce = 100f;
+    public Transform groundCheck;
+
+    private bool grounded = false;
+    private Animator anim;
+    private Rigidbody2D rgdb;
+	
+	void Awake () {
+        anim = GetComponent<Animator>();
+        rgdb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        if(Input.GetButtonDown("Jump") && grounded)
+        {
+            jump = true;
+        }
+	}
+}
